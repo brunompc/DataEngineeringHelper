003a4fb17ff75fd34614777550367ee880652078
Bruno Cipriano <bcipriano@gmail.com>
2022-02-16 01:49:56 +0000

    Remove vertical whitespace

--- files/functions.php
+++ files/functions.php
@@ -11,36 +11,23 @@ print_r($array);
 writeArrayToCSV($array, "teste.csv.txt", "ID,NOME", ",");
 
 function writeArrayToCSV($array, $filename, $header, $separator = ",", $columns_to_exclude = null) {
-	
 	$fp = fopen($filename, "w");
-	
 	$lines = "" . $header . "\n";
-	
 	for($i = 0; $i < sizeof($array); $i++) {
-		
 		$line = "";
 		$first = true;
-		
 		foreach($array[$i] as $column=>$value) {
-			
 			//if($column)
-
 			if(!$first) {
 				$line = $line . $separator;
 			}
-
 			$line = $line . $value;
-			
 			$first = false;
 		}
-		
 		$lines = $lines . $line . "\n";
 	}
-	
 	fwrite($fp, $lines);
-	
 	fclose($fp);
-	
 }
 
 ?>
