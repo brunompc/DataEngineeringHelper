84efc035a0c6e0e23208c40c4ad5b4365cb17b35
Bruno Cipriano <bcipriano@gmail.com>
2022-03-16 23:24:55 +0000

    Add readArrayFromCSV(...) function

--- files/functions.php
+++ files/functions.php
@@ -30,4 +30,20 @@ function writeArrayToCSV($array, $filename, $header, $separator = ",", $columns_
 	fclose($fp);
 }
 
+function readArrayFromCSV($filename, $ignore_first_line = true, $separator = ",") {
+	$fp = fopen($filename, "r");
+	echo $fp;
+	$result = array();
+	$first_line = true;
+	while(!feof($fp)) {
+		$line = fgets($fp);
+		if($first_line && $ignore_first_line) {
+			$first_line = false;
+			continue;
+		}
+		$result[] = explode($separator, $line);
+	}
+	return $result;
+}
+
 ?>
