af32a50e82f4ebfc09b233d0bfb13e756af950cc
Bruno Cipriano <bcipriano@gmail.com>
2022-04-07 23:07:03 +0100

    Change getArrayFromDb() to fetch the data as an associative array

--- databases/functions.php
+++ databases/functions.php
@@ -2,19 +2,12 @@
 
 function getArrayFromDb($host, $database, $username, $password, $sql) {
 	$db = new PDO('mysql:host=' . $host . ';dbname=' . $database, $username, $password);
-	$rs = $db->query($sql);
+	$rs = $db->query($sql, PDO::FETCH_ASSOC);
 	$res = array();
 	foreach($rs as $row) {
 		$res[] = $row;
 	}
 	return $res;
 }
-
-//$array = getArrayFromDb("empresa", "root", "", "select * from funcionarios");
-
-$array = getArrayFromDb("localhost", "dp_old", "root", "", "select * from submission limit 0, 10");
-
-echo "yolo<br>";
-print_r($array);
 
 ?>
